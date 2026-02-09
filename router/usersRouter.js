8a50dd6e07a60eeb464127ac0e1d95a88f784d08
KH.Bidyut <khbidyut31@gmail.com>
2026-02-09 14:07:13 +0600

    complite backend part for add a new user now start frontend

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,14 +1,19 @@
 // external import
 const express = require("express")
 
-const router = express.Router()
-
 // internal import 
-const {getUsers} = require('../controller/usersController')
+const {getUsers, addUser} = require('../controller/usersController')
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse')
+const avatarUpload = require("../middlewares/users/avatarUpload")
+const {addUserValidators,  addUserValidatorsHandler} = require("../middlewares/users/userValidators")
+
+const router = express.Router()
 
 // Users page
 router.get("/",decorateHtmlResponse("Users"), getUsers)
+
+// add user
+router.post("/", avatarUpload, addUserValidators,  addUserValidatorsHandler, addUser )
 
 
 module.exports = router
